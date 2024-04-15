90e9190f45bd0753ce8c7219e5af66d71a6427e9
TABbl <103642830+TABbl@users.noreply.github.com>
2024-04-15 09:21:41 +0300

    upd: report_spec.rb

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe Grup::Calculator do
         expect(result[:count_files]).to eq(14)
         expect(result[:count_pictures]).to eq(5)
         expect(result[:count_classes]).to eq(7)
+
+        report2 = Grup::Calculator.new('spec/new_dir/папка1')
+        result2 = report2.calculate_metrics("spec/test2.txt")
+
+        expect(result2[:count_files]).to eq(3)
+        expect(result2[:count_pictures]).to eq(1)
+        expect(result2[:count_classes]).to eq(0)
       end
     end
 end
